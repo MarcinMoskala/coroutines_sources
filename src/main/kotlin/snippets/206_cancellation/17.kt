625dd1f7bf8becf948ba424a50e2931ca080fd60
Marcin Moskała <marcinmoskala@gmail.com>
2024-04-08 10:17:59 +0200

    Update sources

---
+++ src/main/kotlin/snippets/206_cancellation/17.kt
@@ -0,0 +1,19 @@
+package f_206_cancellation.s_17
+
+import kotlinx.coroutines.*
+
+suspend fun main(): Unit = coroutineScope {
+    launch { // 1
+        launch { // 2, cancelled by its parent
+            delay(2000)
+            println("Will not be printed")
+        }
+        withTimeout(1000) { // we cancel launch
+            delay(1500)
+        }
+    }
+    launch { // 3
+        delay(2000)
+        println("Done")
+    }
+}
