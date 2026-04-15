a4bbbd748644faea1dbf6faa6019993dd238f6ac
Marcin Moskała <marcinmoskala@gmail.com>
2026-04-15 10:31:37 +0200

    Update

--- src/main/kotlin/snippets/205_job/6.kt
+++ src/main/kotlin/snippets/205_job/6.kt
@@ -3,12 +3,12 @@ package f_205_job.s_6
 import kotlinx.coroutines.*
 
 fun main(): Unit = runBlocking {
+    val parentJob: Job = coroutineContext.job
     val job: Job = launch {
         delay(1000)
     }
 
-    val parentJob: Job = coroutineContext.job
     println(job == parentJob) // false
-    val parentChildren: Sequence<Job> = parentJob.children
-    println(parentChildren.first() == job) // true
+    println(parentJob.children.first() == job) // true
+    println(job.parent == parentJob) // true
 }
--- src/main/kotlin/snippets/306_flow_building/1.kt
+++
@@ -1,8 +0,0 @@
-package f_306_flow_building.s_1
-
-import kotlinx.coroutines.flow.*
-
-suspend fun main() {
-    flowOf(1, 2, 3, 4, 5)
-        .collect { print(it) } // 12345
-}
