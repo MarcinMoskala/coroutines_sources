18a69daf99c5e57fcc2f6ce1c9de55d2c6ab6d88
kotlin-books <marcinmoskala@gmail.com>
2026-04-28 13:42:13 +0200

    Update

--- src/main/kotlin/snippets/305_flow_introduction/1.kt
+++
@@ -1,9 +0,0 @@
-package f_305_flow_introduction.s_1
-
-interface Iterable<out T> {
-   operator fun iterator(): Iterator<T>
-}
-
-interface Sequence<out T> {
-   operator fun iterator(): Iterator<T>
-}
--- src/main/kotlin/snippets/308_0_flow_processing/3.kt
+++
@@ -1,9 +0,0 @@
-package f_308_0_flow_processing.s_3
-
-import kotlinx.coroutines.flow.*
-
-suspend fun main() {
-    ('A'..'Z').asFlow()
-        .take(5) // [A, B, C, D, E]
-        .collect { print(it) } // ABCDE
-}
